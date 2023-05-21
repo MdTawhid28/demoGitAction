2f54bd899bc39403e87b21f3df23be0857937c90
mdtawhid <tawhid.md28@gmail.com>
2023-05-21 12:24:33 -0400

    name changes

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -4,7 +4,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        String env=System.getenv("ENVIRONMENT");
+        String env=System.getenv("$ENVIORNMENT");
         String baseUrl="";
         if(env !=null && !(env.equals(""))){
             if(env.equals("dev")){
