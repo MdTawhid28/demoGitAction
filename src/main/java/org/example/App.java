124c93242481c14ee6b4c9f8390bbd026bdc1842
mdtawhid <tawhid.md28@gmail.com>
2023-05-21 00:37:13 -0400

    code added

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -4,6 +4,21 @@ public class App
 {
     public static void main( String[] args )
     {
+        String env=System.getenv("ENVIRONMENT");
+        String baseUrl="";
+        if(env !=null && !(env.equals(""))){
+            if(env.equals("dev")){
+                System.setProperty("baseUrl",System.getenv("myBaseurl1"));
+            }
+            else{
+                System.setProperty("baseUrl",System.getenv("myBaseurl2"));
+            }
+            baseUrl=System.getenv("baseUrl");
+        }
+        else{
+            baseUrl="common";
+        }
 
+        System.out.println(baseUrl);
     }
 }
